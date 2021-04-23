0174be7ff8fe96a52213ae93aaed11f477719680
Shelly-Ling <sontee.ling.chen@gmail.com>
2021-04-23 19:40:48 +0800

    pass - test fetch API data

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,29 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://localhost:3003/posts')
+        .then(function (response) {
+          // 1.handle success
+          console.log(response)
+        })
+        .catch(function (error) {
+          // 2.handle error
+          console.log(error)
+        })
+        .then(function () {
+          // 3.always executed
+        })
+    }
+  }
+}
+</script>
