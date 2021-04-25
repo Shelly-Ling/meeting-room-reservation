788118003936cd5427316aeb6919fcbf411594bd
Shelly-Ling <sontee.ling.chen@gmail.com>
2021-04-25 16:00:48 +0800

    add template in CRoomReservation page

--- src/views/Reservation.vue
+++ src/views/Reservation.vue
@@ -1,19 +1,26 @@
 <template>
-  <v-card class="mx-auto" id="form-wrapper">
-    <ARoomReservation />
-    <BRoomReservation />
-  </v-card>
+  <div>
+    <h1 class="title">您的預約清單</h1>
+    <v-card class="mx-auto" id="form-wrapper">
+      <ARoomReservation />
+      <BRoomReservation />
+      <CRoomReservation />
+    </v-card>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import ARoomReservation from '../components/ARoomReservation.vue'
 import BRoomReservation from '../components/BRoomReservation.vue'
+import CRoomReservation from '../components/CRoomReservation.vue'
+
 
 export default {
   components: {
     ARoomReservation,
-    BRoomReservation
+    BRoomReservation,
+    CRoomReservation
   },
   computed: {
     ...mapState(['currentUser', 'isAuthenticated'])
@@ -25,7 +32,12 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  margin: 30px 0;
+}
 #form-wrapper {
   width: 60vw;
+  height: 83vh;
+  overflow: scroll;
 }
 </style>
